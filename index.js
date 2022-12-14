d9d3d880ffae167dd5b45a344e646a2e81bd633a
suangga2003 <suangga2003@yahoo.com>
2022-12-14 08:43:53 +0700

    bug lagi

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ var corsOptions ={
 }
 
 // app.use(cors(corsOptions));
-// app.use(cors())
+app.use(cors())
 app.use(express.json());
 // app.use("/api/v1/users", userRoutes);
 app.use('/api/v1/auth', authRoutes);
